f9b8567d46606e01b01b9d289114ff3735acf3c3
W10 <W10@DESKTOP-STEVDO8>
2023-02-26 21:36:50 +0300

    Добавьте файлы проекта.

---
+++ WorkWithFiles1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WorkWithFiles1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(12, 118);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(357, 364);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(406, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "проводник";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(392, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(387, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Путь:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(428, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 93);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "пройти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(416, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 58);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "создать папку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(527, 385);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 31);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(513, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Имя папки или файла:";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(885, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 58);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(658, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 93);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "копировать содержимое папки";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(871, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 93);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "вставить содержимое папки";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(642, 433);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 58);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "создать файл";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(658, 95);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 88);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "копировать файл";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(871, 95);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(159, 88);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "вставить файл";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 552);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WorkWithFiles1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkWithFiles1
+{
+    public partial class Form1 : Form
+    {
+        string s, s2, s3, s1;
+        bool b = false,  b2 = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            s= textBox1.Text;
+            if (Directory.Exists(s))
+            {
+                string[] f = Directory.GetFileSystemEntries(s);
+                int i = f.Length;
+                for (int j = 0; j < i; j++)
+                    treeView1.Nodes.Add(f[j]);
+            }
+            else
+                MessageBox.Show(" Папка или файл не найдены! ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+            s2 = textBox2.Text;
+            if ((!Directory.Exists(s + "/" + s2)) && (!File.Exists(s + "/" + s2)))
+            {
+                Directory.CreateDirectory(s + "/" + s2);
+                treeView1.Nodes.Clear();
+                string[] f = Directory.GetFileSystemEntries(s);
+                int i = f.Length;
+                for (int j = 0; j < i; j++)
+                    treeView1.Nodes.Add(f[j]);
+            }
+            else
+                MessageBox.Show("Уже существует с таким название папка или файл!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (b)
+            {
+                s = textBox1.Text;
+                Copy(s3, s);
+                void Copy(string from, string to)
+                {
+                    Directory.CreateDirectory(to);
+                    foreach (string s4 in Directory.GetFiles(from))
+                    {
+                        string s5 = to + "//" + Path.GetFileName(s4);
+                        if(!File.Exists(s5))
+                            File.Copy(s4, s5);
+                    }
+                    foreach (string s6 in Directory.GetDirectories(from))
+                    {
+                        if(!Directory.Exists(s6))
+                            Copy(s6, to + "//" + Path.GetFileName(s6));
+                    }
+                }
+                treeView1.Nodes.Clear();
+                string[] f = Directory.GetFileSystemEntries(s);
+                int i = f.Length;
+                for (int j = 0; j < i; j++)
+                    treeView1.Nodes.Add(f[j]);
+            }
+            else
+                MessageBox.Show("Буфер обмена пуст!");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (b2)
+            {
+                s= textBox1.Text + "//" + Path.GetFileName(s1);
+                if (!File.Exists(s))
+                {
+                    File.Copy(s1, s);
+                    treeView1.Nodes.Clear();
+                    s = textBox1.Text;
+                    string[] f = Directory.GetFileSystemEntries(s);
+                    int i = f.Length;
+                    for (int j = 0; j < i; j++)
+                        treeView1.Nodes.Add(f[j]);
+                }
+                else
+                    MessageBox.Show("Файл с таким именем уже существует!");
+            }
+            else
+                MessageBox.Show("Буфер обмена пуст!");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+            s2 = textBox2.Text;
+            if ((!Directory.Exists(s + "/" + s2)) && (!File.Exists(s + "/" + s2)))
+            {
+                File.Create(s + "/" + s2);
+                treeView1.Nodes.Clear();
+                string[] f = Directory.GetFileSystemEntries(s);
+                int i = f.Length;
+                for (int j = 0; j < i; j++)
+                    treeView1.Nodes.Add(f[j]);
+            }
+            else
+                MessageBox.Show("Уже существует с таким название папка или файл!");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            s1 = textBox1.Text;
+            b2 = true;
+            if (!File.Exists(s1))
+            {
+                b2 = false;
+                s1 = "";
+                MessageBox.Show("Файл с таким именем не найдена!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            b = true;
+            s3 = textBox1.Text;
+            if (!Directory.Exists(s3))
+            {
+                b = false;
+                s3 = "";
+                MessageBox.Show(" Папка с таким именем не найдена! ");
+            }
+                
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+            s2 = textBox2.Text;
+            if (Directory.Exists(s + "/" + s2))
+            {
+                Directory.Delete(s + "/" + s2, true);
+                treeView1.Nodes.Clear();
+                string[] f = Directory.GetFileSystemEntries(s);
+                int i = f.Length;
+                for (int j = 0; j < i; j++)
+                    treeView1.Nodes.Add(f[j]);
+            }
+            else if(File.Exists(s + "/" + s2)){
+                File.Delete(s + "/" + s2);
+                treeView1.Nodes.Clear();
+                string[] f = Directory.GetFileSystemEntries(s);
+                int i = f.Length;
+                for (int j = 0; j < i; j++)
+                    treeView1.Nodes.Add(f[j]);
+            }
+            else
+                MessageBox.Show("Не существует ни файла, ни папки с таким названием!");
+        }
+    }
+}
